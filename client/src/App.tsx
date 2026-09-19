3a08a4f45dbaeedd296584262fe61045cb8d924b
profitpilotsa <profitpilotsa@gmail.com>
2026-09-19 23:55:14 +0300

    Add original client application source

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import ErrorBoundary from "./components/ErrorBoundary";
+import Home from "./pages/Home";
+
+export default function App() {
+  return (
+    <ErrorBoundary>
+      <ThemeProvider defaultTheme="dark">
+        <TooltipProvider>
+          <Toaster position="bottom-right" theme="dark" />
+          <Home />
+        </TooltipProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
+  );
+}
